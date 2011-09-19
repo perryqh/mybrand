db3a1c887a8ad76318108715fb665c5eb152c517
Perry Hertler <perry@hertler.org>
2011-09-19 09:17:38 -0700

    Initial checkin

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mybrand::Application.config.secret_token = 'a16126217ba619f68299e92cd274c48aac09f631887f2b4aed67789a14d5eff10df9b05799fb4322370bd467edafef20c42ece06d2d2e2c398691106e7e3e2ce'
