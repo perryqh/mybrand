ff1243ebac0c48e671d3a2a0206ec34786bcf130
Perry Hertler <perry@hertler.org>
2011-09-27 14:39:00 -0700

    just using spec

--- spec/controllers/dashboard_controller_spec.rb
+++ spec/controllers/dashboard_controller_spec.rb
@@ -4,7 +4,7 @@ describe DashboardController do
   describe "GET index" do
     it "should render index template" do
       get :index
-      
+
       response.should be_success
       response.should render_template(:index)
     end
