6438e398dc5f3b10eeb2490e7f4079d8d09d214b
Perry Hertler <perry@hertler.org>
2011-09-27 14:30:02 -0700

    personalizing root page

--- spec/controllers/dashboard_controller_spec.rb
+++ spec/controllers/dashboard_controller_spec.rb
@@ -1,5 +1,12 @@
 require 'spec_helper'
 
 describe DashboardController do
-
+  describe "GET index" do
+    it "should render index template" do
+      get :index
+      
+      response.should be_success
+      response.should render_template(:index)
+    end
+  end
 end
