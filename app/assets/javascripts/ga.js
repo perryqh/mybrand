a6301f0c2d41fc01ba1243eb5e35286700850c0a
Perry Hertler <perry@hertler.org>
2011-10-01 14:37:40 -0700

    fixing unobtrusive ga

--- app/assets/javascripts/ga.js
+++ app/assets/javascripts/ga.js
@@ -1,12 +1,13 @@
 jQuery(document).ready(function($) {
-  var gaTrackCode = "UA-7211958-1";
   var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
 
   jQuery.getScript(gaJsHost + "google-analytics.com/ga.js", function(){
-    try {
-      var pageTracker = _gat._getTracker("UA-7211958-1");
-      pageTracker._trackPageview();
-    } catch(err) {}
+    _gaq.push(['_setAccount', 'UA-7211958-1']);
+    _gaq.push(['_trackPageview']);
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
   });
 });
+
 
