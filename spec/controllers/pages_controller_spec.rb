e406a6011c83e315733a0d9a39fbb69321da5cee
Perry Hertler <perry@hertler.org>
2011-10-06 11:06:11 -0700

    empty layout for 37sigs

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -8,5 +8,14 @@ describe PagesController do
         response.should render_template(page)
       end
     end
+
+    describe 'layout_for_page' do
+      it "use subdomain to determing layout" do
+        controller.send(:layout_for_page).should == 'application'
+
+        request.host = "37signals.example.com"
+        controller.send(:layout_for_page).should == 'empty'
+      end
+    end
   end
 end
