53d09121d4593b91ee342b1b3275b07a6576a071
Perry Hertler <perry@hertler.org>
2011-10-31 17:32:43 -0500

    37signals cover

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,3 +1,9 @@
 class PagesController < HighVoltage::PagesController
+  layout :layout_for_page
 
+  private
+  def layout_for_page
+    return 'empty' if request.subdomain == '37signals'
+    'application'
+  end
 end
